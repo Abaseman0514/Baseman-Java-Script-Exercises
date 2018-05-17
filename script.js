71ca6ea3a2854067b0ac31bd199522a6bbe57734
Ariana <arianamb@yahoo.com>
2018-05-17 01:53:35 -0400

    Baseman Java Script Exercises for prework

---
+++ script.js
@@ -0,0 +1,59 @@
+// My Java was working, as far as I could take these, in repl.it. But the actual file would not bring up a browser and I double and triple checked my link to html.
+// Exercise 2 Got stuck in the continuous loop -- it wouldn't continue asking.
+// Exercise 3 Could not figure out how to repeat the question after the initial follow-up. Or have the exclaimation point keep expanding.
+// Exercise 4 seems to work! Just not as a repeat pop-up.
+
+
+// EXERCISE 1
+
+//var name = prompt("Enter a number!");
+
+//var total = 0;
+//for (var i = 1; i <= name; i++) {
+//total = total + i;
+//}
+//console.log(total);
+
+//EXERCISE 2
+
+//var name = prompt("Enter a number!");
+
+//var total = 0;
+//for (var i = 1; i <= name; i++) {
+//total = total + i;
+//}
+//console.log(total);
+
+
+//EXERCISE 3
+
+//var question;
+//var repeat
+//var x=1;
+
+ //question = prompt("Would you like to print your name?")
+//while (question !== "yes");
+//console.log("Hello. My name is Adam");
+
+//if (repeat == "yes");
+ //repeat = prompt("Would you like to print your name again?");
+
+//var output = "Hello. My name is Adam"
+//{
+//output = output+"!";
+//console.log(output)
+//}
+//x++;
+
+//EXERCISE 4
+
+var menu = prompt("What time of day is it? Morning, noon or night?")
+
+if (menu === "Morning")
+  console.log("Since it's morning, breakfast is the best option. We recommend coffee and an English Muffin.");
+
+  else if  (menu === "noon")
+  console.log("Since it's midday, lunch is the best option. We recommend a sandwich.");
+
+  else if (menu === "night")
+  console.log("Since it's night, dinner is the best option. We recommend something off the grill.");
