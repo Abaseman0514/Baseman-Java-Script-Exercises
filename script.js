0109235b0e47779e15ba3d8cfe840b125c5f5e9f
Ariana <arianamb@yahoo.com>
2018-05-22 22:06:06 -0400

    Final Prework Submission - JavaScript - A Baseman

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// My Java was working, as far as I could take these, in repl.it. But the actual file would not bring up a browser and I double and triple checked my link to html.
-// Exercise 2 Got stuck in the continuous loop -- it wouldn't continue asking.
-// Exercise 3 Could not figure out how to repeat the question after the initial follow-up. Or have the exclaimation point keep expanding.
-// Exercise 4 seems to work! Just not as a repeat pop-up.
-
-
 // EXERCISE 1
 
 //var name = prompt("Enter a number!");
@@ -16,44 +10,40 @@
 
 //EXERCISE 2
 
-//var name = prompt("Enter a number!");
+//var question = prompt("Do you want to play?");
+//var answer = prompt("Enter a word");
+//var response = "yes";
+//var repeat = ("Do you want to play again?");
 
-//var total = 0;
-//for (var i = 1; i <= name; i++) {
-//total = total + i;
-//}
-//console.log(total);
+//do {
+  //answer = answer
+  //prompt(repeat);
+    //console.log(answer);
+//} while (repeat === true);
 
 
 //EXERCISE 3
 
-//var question;
-//var repeat
-//var x=1;
+//var question = prompt("Would you like to print your name?");
 
- //question = prompt("Would you like to print your name?")
-//while (question !== "yes");
-//console.log("Hello. My name is Adam");
-
-//if (repeat == "yes");
- //repeat = prompt("Would you like to print your name again?");
-
-//var output = "Hello. My name is Adam"
-//{
-//output = output+"!";
-//console.log(output)
+//var i = "!";
+//while (question === "yes") {
+//  console.log("Hello. My name is Adam" + i);
+//i++;
+//name = prompt("Would you like to print again?");
 //}
-//x++;
+
 
 //EXERCISE 4
 
-var menu = prompt("What time of day is it? Morning, noon or night?")
+//var menu = prompt("What time of day is it? Morning, noon or night?")
 
-if (menu === "Morning")
-  console.log("Since it's morning, breakfast is the best option. We recommend coffee and an English Muffin.");
-
-  else if  (menu === "noon")
-  console.log("Since it's midday, lunch is the best option. We recommend a sandwich.");
-
-  else if (menu === "night")
-  console.log("Since it's night, dinner is the best option. We recommend something off the grill.");
+//if (menu === "Morning"){
+  //console.log("Since it's morning, breakfast is the best option. We recommend coffee and an English Muffin.");
+//}
+//  else if  (menu === "noon"){
+  //console.log("Since it's midday, lunch is the best option. We recommend a sandwich.");
+//}
+  //else if (menu === "night"){
+  //console.log("Since it's night, dinner is the best option. We recommend something off the grill.");
+//}
